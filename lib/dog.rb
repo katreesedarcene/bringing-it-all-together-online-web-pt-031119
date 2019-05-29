c559498880334aee50db8d407c46eed0024b72b5
Kat B <katreesebarnes@gmail.com>
2019-05-29 23:45:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,12 @@ class Dog
   attr_accessor :name, :breed, :id
   
   
-    def initialize(id: nil, name:, breed:)
-      @name = name
-      @breed = breed 
-      @id = id
-    end
+   def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
     
   def self.create_table
     sql =  <<-SQL 
