c64db63a0ce5d467d42181896f7657199b364872
Kat B <katreesebarnes@gmail.com>
2019-05-29 23:27:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,14 +25,16 @@ class Dog
      DB[:conn].execute(sql) 
   end
   
-   def save
+  def save
+  if self.id
+    self.update
+  else
     sql = <<-SQL
-      INSERT INTO dogs (name, breed)
+      INSERT INTO dogs(name, album)
       VALUES (?, ?)
     SQL
-
-    DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    DB[:conn].execute(sql, self.name, self.album)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
   
   
