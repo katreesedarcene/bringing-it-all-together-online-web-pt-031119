7007d0a6755ce23c835e4cf3192c9029c19613c0
Kat B <katreesebarnes@gmail.com>
2019-05-29 23:48:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -85,6 +85,12 @@ class Dog
     end
     dog
   end
+  
+   def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Dog.new(result[0], result[1], result[2])
+  end
 
 
 
